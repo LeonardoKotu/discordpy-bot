f2b924a799e634adaa2d3bd4e112573c534e6eb9
f1sher <100369987+LeonardoKotu@users.noreply.github.com>
2023-05-05 15:04:04 +0200

    Add files via upload

---
+++ cogs/info.py
@@ -0,0 +1,101 @@
+import disnake
+from disnake.ext import commands
+from bot import bot
+
+class Button(disnake.ui.View):
+
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @disnake.ui.button(
+        custom_id="btn1",
+        label='',
+        emoji="<:console:1102333817789882378>"
+    )
+
+
+    async def callback_1(self, button, inter):
+        if button.custom_id == 'btn1':
+            member = inter.author
+            role = inter.guild.get_role(1102006225736056882)
+            await member.add_roles(role)
+        await inter.response.send_message(f"Вы получили роль **Ивента.**", ephemeral=True)
+
+
+    @disnake.ui.button(
+        custom_id="btn2",
+        label='',
+        emoji="<:newspaper:1102333814933557268>"
+    )
+
+
+    async def callback_2(self, button, inter):
+        if button.custom_id == 'btn2':
+            member = inter.author
+            role = inter.guild.get_role(1102006228722389133)
+            await member.add_roles(role)
+        await inter.response.send_message(f"Вы получили роль **Новости и обновление.**", ephemeral=True)
+
+
+
+    @disnake.ui.button(
+        custom_id="btn3",
+        label='',
+        emoji="<:suitcase:1102333812207259820>"
+    )
+
+
+    async def callback_3(self, button, inter):
+        if button.custom_id == 'btn3':
+            member = inter.author
+            role = inter.guild.get_role(1102006227363450950)
+            await member.add_roles(role)
+        await inter.response.send_message(f"Вы получили роль **Набор.**", ephemeral=True)
+
+    @disnake.ui.button (
+        custom_id="btn4",
+        label="",
+        emoji="<:bellring:1102715523239772180>"
+    )
+
+    async def callback_4(self, button, inter):
+        if button.custom_id == "btn4":
+            member = inter.author
+            role = inter.guild.get_role(1102009783306965034)
+            await member.add_roles(role)
+        await inter.response.send_message(f"Вы получили роль **Бамп.**", ephemeral=True)
+
+
+class Info(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        bot.add_view(Button())
+
+
+    @commands.slash_command()
+    async def info(self, ctx):
+
+        embed = disnake.Embed(
+            title = "<a:green_sparkling_star:1101997680055558197> Выбор роли оповещений Amnesia",
+            description=
+            """
+            ｡︎ »︎ <:console:1102333817789882378> - игры/ивенты
+            ｡︎ »︎ <:newspaper:1102333814933557268> - новости и обновление 
+            ｡︎ »︎ <:suitcase:1102333812207259820> - набор на стафф
+            ｡︎ »︎ <:bellring:1102715523239772180> - [бампы](https://discord.com/channels/1100858771347095755/1101908856931623042)
+            """,
+            colour=0xe513f0
+        )
+
+        embed.set_image(url="https://i.pinimg.com/564x/ef/26/dd/ef26dde66380a72c8ac9b7571530820e.jpg")
+
+        await ctx.send(view=Button(), embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Info(bot))
---
+++ cogs/ticket.py
@@ -0,0 +1,161 @@
+import disnake
+from disnake.ext import commands
+from bot import bot
+# Наследуем модальное окно
+class Menu(disnake.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @disnake.ui.select(
+        custom_id="select",
+        placeholder="🔑 Выберите тип обращение",
+        min_values=1,
+        max_values=1,
+        options = [
+            disnake.SelectOption(
+
+                label="Жалоба",
+                description = "Жалоба на стафф/участник",
+                emoji="🔒"
+            ),
+
+            disnake.SelectOption(
+                label="Идея & Баги",
+                description="Предложение для развитие сервера/нахождение багов",
+                emoji="🔒"
+            ),
+
+            disnake
+            .SelectOption(
+                label="Снять выбор",
+                emoji="❗"
+            ),
+
+        ]
+        )
+
+
+    # Обработка ответа, после отправки модального окна
+    async def callback(self, select, inter):
+
+        # если пользователь нажал на жалобу
+        if select.values[0] == "Жалоба":
+
+            cat = disnake.utils.get(inter.guild.categories, name="—  •  🔐 Тикеты") # категория
+            global channel
+            channel = await inter.guild.create_text_channel(f'🔖обращение-от-{inter.author}', category=cat) # создаем канал на этой категории
+
+            '''отправляем эмбед'''
+            embed_zaloba = disnake.Embed(
+                title = "Обращение на тему `жалоба`",
+                description=
+                """
+                > Опишите вашу проблему, *причину* и *ник* человека которые вы захотили **пожаловаться**.
+                > За ложный тикет, вы будете **замьючены.**
+                """,
+                colour = 0x34582
+            )
+
+            # настройка
+            
+            embed_zaloba.set_footer(text="🌙 /close чтобы закрыть тикет.") # футер
+            await channel.send(content=f"{inter.user.mention} / <@&1100946600886534177>", embed=embed_zaloba) # отправка сообщений, после нажатий меню
+            
+            await channel.set_permissions(inter.guild.default_role, read_messages=False) # это установит роле @everyone запрет на просмотр чата
+            await channel.set_permissions(inter.user, read_messages=True, send_messages=True) # указываем права
+
+            await inter.response.send_message(f"Для вас создана канал обращение - <#{channel.id}>, желаем удачи!", ephemeral=True) # сообщение о том что все успешно
+
+            log_channel = inter.guild.get_channel(1100900722855387196)
+
+            new_log = disnake.Embed(
+            title = f"{inter.user}, создал тикет",
+            description=
+            f"""
+            {inter.user.mention} создал тикет, на канале <#{channel.id}>
+            Тип: {select.values[0]}
+            """
+        )
+            await log_channel.send(embed=new_log)
+
+
+        if select.values[0] == "Идея & Баги":
+
+            cat = disnake.utils.get(inter.guild.categories, name="—  •  🔐 Тикеты") # категория
+            channel = await inter.guild.create_text_channel(f'💡обращение-от-{inter.author}', category=cat) # создаем канал на этой категории
+
+            '''отправляем эмбед'''
+            embed_zaloba = disnake.Embed(
+                title = "Обращение на тему `Идея & Баги`",
+                description=
+                """
+                > Расскажите **идею** для развитие сервера, или **баг** в котором вы заметили на сервере.
+                > За ложный тикет, вы будете **замьючены.**
+                """,
+                colour = 0x39e582
+            )
+
+            # настройка
+            
+            embed_zaloba.set_footer(text="🌙 /close чтобы закрыть тикет.") # футер
+            await channel.send(content=f"{inter.user.mention} / <@&1100946600886534177>", embed=embed_zaloba) # отправка сообщений, после нажатий меню
+            
+            await channel.set_permissions(inter.guild.default_role, read_messages=False) # это установит роле @everyone запрет на просмотр чата
+            await channel.set_permissions(inter.user, read_messages=True, send_messages=True) # указываем права
+
+            await inter.response.send_message(f"Для вас создана канал обращение - <#{channel.id}>, желаем удачи!", ephemeral=True) # сообщение о том что все успешно
+
+            log_channel = inter.guild.get_channel(1100900722855387196)
+
+            new_log = disnake.Embed(
+            title = f"{inter.user}, создал тикет",
+            description=
+            f"""
+            {inter.user.mention} создал тикет, на канале <#{channel.id}>
+            Тип: {select.values[0]}
+            """
+        )
+            await log_channel.send(embed=new_log)
+
+
+        if select.values[0] == "Снять выбор":
+
+            await inter.response.send_message("Успешно.", ephemeral=True) # сообщение о том что все успешно
+
+
+
+class Titckets(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        bot.add_view(Menu())
+
+    @commands.slash_command()    
+    async def ticket(self, inter):
+        
+        embed = disnake.Embed(
+            title = "Помоги нам стать лучше вместе с AMNESIA!",
+            description=
+            """
+            > `-` Это канал поддержки. Выберите тип *обращение*, который вы хотите **обратиться**.
+
+            """,
+            colour = 0x2f3136
+
+        )
+
+        embed.set_footer(text="Просим не создавать обращении бессмысленно.")
+
+        embed.set_thumbnail(url="https://media.discordapp.net/attachments/1101908582376689834/1101910941223899247/help-web-button.png")
+
+        await inter.send(embed=embed, view=Menu())
+
+    @commands.slash_command(description="Закрыть тикет")
+    async def close(self, inter):
+        await channel.delete()
+
+def setup(bot):
+    bot.add_cog(Titckets(bot))
